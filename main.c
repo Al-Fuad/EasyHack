3307c82ce9c9c61ce52c9b40acd67a558e110301
Nur <s.m.al-fuadnur@Ss-MacBook-Air.local>
2023-09-24 00:27:08 +0600

    wafw00f added into main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /* 
  * ||===================================================||
- * ||===================== EasyHack =====================||
+ * ||===================== EasyHack ====================||
  * ||===================================================||
  * ||= Project-Description: This is a tool handler for =||
  * ||= ethical hacking tools. The tool, Nmap, John The =||
@@ -23,12 +23,13 @@
 #include"src/tools/nmap.h"
 #include"src/tools/dirb.h"
 #include"src/tools/lbd.h"
+#include"src/tools/wafw00f.h"
 
 int main()
 {
     int tool;
     banner("Easy Hack");
-    printf("Tools :\n1|| Nmap\n2|| Dirb\n3|| LBD\n4|| WafW00f\n5|| DIRSearch\n6|| NetCat\n7|| JohnTheRipper\n8|| SQLMap\nEnter tool number : ");
+    printf("Tools :\n1|| Nmap\n2|| Dirb\n3|| LBD\n4|| WafW00f\n5|| NetCat\n6|| JohnTheRipper\n7|| SQLMap\nEnter tool number : ");
     scanf("%d",&tool);
     if(tool == 1){
         nmap();
@@ -38,6 +39,9 @@ int main()
     }
     else if(tool == 3){
         lbd();
+    }
+    else if(tool == 4){
+        wafw00f();
     }
     return 0;
 }
