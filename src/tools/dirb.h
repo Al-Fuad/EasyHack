c3401ade853e2e905c49cf092ef8cea02f917881
Nur <s.m.al-fuadnur@Ss-MacBook-Air.local>
2023-12-09 22:16:26 +0600

    Dirb updated.

--- src/tools/dirb.h
+++ src/tools/dirb.h
@@ -8,48 +8,54 @@ void dirb(){
     strcpy(dirb.name,"dirb");
     while(true){
         banner("Dirb");
-        printf("1|| Set Target\t");
-        if(!(strlen(dirb.target) == 0)){
+        printf("1|| Set Target\t\t\t\t\t\t\t");
+        if(!(strlen(dirb.target) == 0))
             printf("✓");
-        }
+        else
+            printf("□");
         printf("\n\n");
-        printf("2|| Set Wordlist\t");
-        if(!(strlen(dirb.wordlists) == 0)){
+        printf("2|| Set Wordlist\t\t\t\t\t\t");
+        if(!(strlen(dirb.wordlists) == 0))
             printf("✓");
-        }
+        else
+            printf("□");
         printf("\n\n");
-        printf("3|| Set Speed\t");
-        if(!(strlen(dirb.time) == 0)){
+        printf("3|| Set Speed\t\t\t\t\t\t\t");
+        if(!(strlen(dirb.time) == 0))
             printf("✓");
-        }
+        else
+            printf("□");
         printf("\n\n");
-        printf("4|| Set Extention\t");
-        if(!(strlen(dirb.extention) == 0)){
+        printf("4|| Set Extention\t\t\t\t\t\t");
+        if(!(strlen(dirb.extention) == 0))
             printf("✓");
-        }
+        else
+            printf("□");
         printf("\n\n");
-        printf("5|| Set Header\t");
-        if(!(strlen(dirb.header) == 0)){
+        printf("5|| Set Header\t\t\t\t\t\t\t");
+        if(!(strlen(dirb.header) == 0))
             printf("✓");
-        }
+        else
+            printf("□");
         printf("\n\n");
-        printf("6|| Set Ignore Response Code\t");
-        if(!(strlen(dirb.ignoreCode) == 0)){
+        printf("6|| Set Ignore Response Code\t\t\t\t\t");
+        if(!(strlen(dirb.ignoreCode) == 0))
             printf("✓");
-        }
+        else
+            printf("□");
         printf("\n\n");
         printf("0|| Next(Enter target first. Otherwise it dosn't work.)\n\n");
         printf("Enter your choice: ");
         scanf("%d", &op);
         if(op == 0){
-            if(!(strlen(dirb.target) == 0)){
+            if(strlen(dirb.target) != 0){
                 printf("\nDo you want to save?\n\n1|| Yes\n\n2|| No\n\nEnter your choice: ");
                 scanf("%d", &op2);
                 if(op2 == 1){
                     char output[100];
                     printf("\nEnter file name: ");
                     scanf("%s",output);
-                    strcpy(dirb.outputType, "-o");
+                    strcpy(dirb.outputFile, "-o src/saves/dirb/");
                     strcpy(dirb.outputFile, output);
                     strcat(dirb.outputFile, ".txt");
                 }
@@ -57,16 +63,16 @@ void dirb(){
             }
         }
         else if(op == 1){
-            printf("\nEnter the target : ");
+            printf("\nEnter the target: ");
             scanf("%s",dirb.target);
         }
         else if(op == 2){
             char word[100];
             printf("\nWordlists:\n\n");
-            system("ls src/dirbWordlists/");
+            system("ls src/wordlists/dirb/");
             printf("\n\nEnter wordlist name: ");
             scanf("%s", word);
-            strcpy(dirb.wordlists, "-w src/dirbWordlists/");
+            strcpy(dirb.wordlists, "-w src/wordlists/dirb/");
             strcat(dirb.wordlists, word);
         }
         else if(op == 3){
