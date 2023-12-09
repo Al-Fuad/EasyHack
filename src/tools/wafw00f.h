429be45d78b3f0dfc7e9a3270c481334413c69e9
Nur <s.m.al-fuadnur@Ss-MacBook-Air.local>
2023-12-10 01:24:52 +0600

    Update wafw00f.h

--- src/tools/wafw00f.h
+++ src/tools/wafw00f.h
@@ -2,11 +2,10 @@
 #include "tools.h"
 
 void wafw00f(){
-    banner("wafw00f");
-    tool wafw00f;
-    strcpy(wafw00f.cmd,"wafw00f ");
+    banner("WafW00f");
+    tool wafw00f = {'\0'};
+    strcpy(wafw00f.name,"wafw00f");
     printf("\nEnter the target : ");
     scanf("%s",wafw00f.target);
-    strcat(wafw00f.cmd, wafw00f.target);
-    run(wafw00f.cmd);
+    cc(wafw00f);
 }
