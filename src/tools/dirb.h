4d38479a19cb5d801b9929051a5c1b114783720b
Nur <s.m.al-fuadnur@Ss-MacBook-Air.local>
2023-12-10 05:36:18 +0600

    Add features

--- src/tools/dirb.h
+++ src/tools/dirb.h
@@ -8,52 +8,24 @@ void dirb(){
     strcpy(dirb.name,"dirb");
     while(true){
         banner("Dirb");
-        printf("1|| Set Target\t\t\t\t\t\t\t");
-        if(!(strlen(dirb.target) == 0))
-            printf("☑");
-        else
-            printf("□");
-        printf("\n\n");
-        printf("2|| Set Wordlist\t\t\t\t\t\t");
-        if(!(strlen(dirb.wordlists) == 0))
-            printf("☑");
-        else
-            printf("□");
-        printf("\n\n");
-        printf("3|| Set Speed\t\t\t\t\t\t\t");
-        if(!(strlen(dirb.time) == 0))
-            printf("☑");
-        else
-            printf("□");
-        printf("\n\n");
-        printf("4|| Set Extention\t\t\t\t\t\t");
-        if(!(strlen(dirb.extention) == 0))
-            printf("☑");
-        else
-            printf("□");
-        printf("\n\n");
-        printf("5|| Set Header\t\t\t\t\t\t\t");
-        if(!(strlen(dirb.header) == 0))
-            printf("☑");
-        else
-            printf("□");
-        printf("\n\n");
-        printf("6|| Set Ignore Response Code\t\t\t\t\t");
-        if(!(strlen(dirb.ignoreCode) == 0))
-            printf("☑");
-        else
-            printf("□");
-        printf("\n\n");
-        printf("0|| Next(Enter target first. Otherwise it dosn't work.)\n\n");
-        printf("Enter your choice: ");
-        scanf("%d", &op);
+        lineMark(1,"Set Target",strlen(dirb.target));
+        lineMark(2,"Set Wordlist",strlen(dirb.wordlists));
+        lineMark(3,"Set Speed",strlen(dirb.time));
+        lineMark(4,"Set Extention",strlen(dirb.extention));
+        lineMark(5,"Set Header",strlen(dirb.header));
+        lineMark(6,"Set Ignore Response Code",strlen(dirb.ignoreCode));
+        line(0, "Next(Enter target first. Otherwise it dosn't work.)");
+        op = intInput();
         if(op == 0){
             if(strlen(dirb.target) != 0){
-                printf("\nDo you want to save?\n\n1|| Yes\n\n2|| No\n\nEnter your choice: ");
-                scanf("%d", &op2);
+                heading("Do you want to save?");
+                line(1,"Yes");
+                line(2,"No");
+                op2 = intInput();
                 if(op2 == 1){
                     char output[100];
-                    printf("\nEnter file name: ");
+                    heading("Enter file name");
+                    easyHack();
                     scanf("%s",output);
                     strcpy(dirb.outputFile, "-o src/saves/dirb/");
                     strcpy(dirb.outputFile, output);
@@ -63,46 +35,51 @@ void dirb(){
             }
         }
         else if(op == 1){
-            printf("\nEnter the target: ");
+            heading("Enter the target");
+            easyHack();
             scanf("%s",dirb.target);
         }
         else if(op == 2){
             char word[100];
-            printf("\nWordlists:\n\n");
+            heading("Wordlists");
             system("ls src/wordlists/dirb/");
-            printf("\n\nEnter wordlist name: ");
+            heading("Enter wordlist name");
+            easyHack();
             scanf("%s", word);
             strcpy(dirb.wordlists, "-w src/wordlists/dirb/");
             strcat(dirb.wordlists, word);
         }
         else if(op == 3){
             char speed[100];
-            printf("\nEnter speed in milisecond: ");
+            heading("Enter speed in milisecond");
+            easyHack();
             scanf("%s", speed);
             strcpy(dirb.time, "-z ");
             strcat(dirb.time, speed);
         }
         else if(op == 4){
             char exe[100];
-            printf("\nEnter extention name: ");
+            heading("Enter extention name");
+            easyHack();
             scanf("%s", exe);
             strcpy(dirb.extention, "-X ");
             strcat(dirb.extention, exe);
         }
         else if(op == 5){
             char head[100];
-            printf("\nEnter header name: ");
+            heading("Enter header name");
+            easyHack();
             scanf("%s", head);
             strcpy(dirb.header, "-H ");
             strcat(dirb.header, head);
         }
         else if(op == 6){
             char icode[100];
-            printf("\nEnter ignore response code: ");
+            heading("Enter ignore response code");
+            easyHack();
             scanf("%s", icode);
             strcpy(dirb.ignoreCode, "-N ");
             strcat(dirb.ignoreCode, icode);
-
         }
     }
 
