ff5518d76df08019b588d50dc74f0482da238c7e
Nur <s.m.al-fuadnur@Ss-MacBook-Air.local>
2023-12-12 13:36:14 +0600

    Add os.h

---
+++ os.h
@@ -0,0 +1,19 @@
+// To find os for specific cmd/shall command
+#if defined(__APPLE__)
+    char * setUpFiglet = "brew install figlet";
+    char * setUpNmap = "brew install nmap";
+    char * setUpDirb = "brew install sidaf/pentest/dirb";
+    char * setUpJohn = "brew install sidaf/pentest/john-jumbo";
+    char * setUpWafw00f = "brew install sidaf/pentest/wafw00f";
+
+    // LBD is not available for mac
+    // char * setUpLBD = "";
+
+#elif defined(__linux)
+    char * setUpFiglet = "sudo apt-get install figlet";
+    char * setUpNmap = "sudo apt-get install nmap";
+    char * setUpDirb = "sudo apt-get install dirb";
+    char * setUpJohn = "sudo apt-get install john";
+    char * setUpWafw00f = "sudo apt-get install wafw00f";
+    char * setUpLBD = "sudo apt-get install ldb-tools";
+#endif
