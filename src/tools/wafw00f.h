bb5390614f7d331435c7555150033bf24a2bf8a5
Nur <s.m.al-fuadnur@Ss-MacBook-Air.local>
2023-12-12 13:42:04 +0600

    Update wafw00f.h

--- src/tools/wafw00f.h
+++ src/tools/wafw00f.h
@@ -2,10 +2,26 @@
 #include "tools.h"
 
 void wafw00f(){
-    banner("WafW00f");
     tool wafw00f = {'\0'};
     strcpy(wafw00f.name,"wafw00f");
-    printf("\nEnter the target : ");
-    scanf("%s",wafw00f.target);
-    cc(wafw00f);
+    int op;
+    while(true){
+        banner("WafWOOf");
+        heading("Option");
+        lineMark(1,"Set Target",strlen(wafw00f.target));
+        line(2,"Next(Enter target first. Otherwise it dosn't work.)");
+        line(0,"Back");
+        op = intInput();
+        if(op == 1){
+            heading("Enter The Target");
+            stringInput(wafw00f.target);
+        }
+        else if(op == 2 && strlen(wafw00f.target) != 0){
+            run(wafw00f);
+            break;
+        }
+        else if(op == 0){
+            break;
+        }
+    }
 }
